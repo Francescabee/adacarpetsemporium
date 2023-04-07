6968443cb281d57d48098146923f5bd32534ed19
Francesca <129342101+Francescabee@users.noreply.github.com>
2023-04-07 10:42:13 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+# Refer to the README.md file in this project for a description 
+# of the challenge and requirements. When viewing the README.md, 
+# replit will show an option to Open Preview, which will open a 
+# more readable view.
+
+# The Python module math is imported and available to use but not required.
+# Please do not import any additional modules (such as NumPy).
+
+# import math
+
+# def carpet_quote_estimator(room, carpet_type, options, promo_code):
+    # pass
+
+# You may call carpet_quote_estimator with your own arguments here.
+
+def carpet_quote_estimator(room, carpet_type, options, promo_code)
+
+# For example, if the following line is uncommented, and the Run button
+# is clicked, it will call carpet_quote_estimator with the listed
+# parameters. The supplied example uses the same arguments as the
+# test Challenge01.
+    
+# carpet_quote_estimator([10, 10], "berber", ["delivery", "installation"], "")
+
+# The challenges are provided as a set of tests that can be run 
+# from the Tests panel at the left (the icon looks like a check 
+# mark). Clicking the Run tests button will run the Challenge 
+# inputs against your code, displaying either a success message, 
+# or a message about what was expected and what was observed.
+
+# After running the tests, it may be necessary to open a Console
+# tab if the Test panel replaces it on the right side of the screen.
